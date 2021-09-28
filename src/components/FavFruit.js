ba2f8ede8f3edd9bd74bd5b76c0a38ba79c70657
yousef mando <85874237+yousefmamdo@users.noreply.github.com>
2021-09-28 11:32:24 +0300

    Initial commit

---
+++ src/components/FavFruit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class FavFruit extends React.Component {
+  render() {
+    return(
+      <>
+        <h1>My Favorite Fruits</h1>
+      </>
+    )
+  }
+}
+
+export default FavFruit;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Home extends React.Component {
+
+  render() {
+    return (
+      <>
+        <h1>API Fruits</h1>
+      </>
+    )
+  }
+}
+
+export default Home;
